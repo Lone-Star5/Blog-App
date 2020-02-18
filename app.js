53b318814a8abc77a5b5e830ef71592d7d1a986e
Lone-Star5 <amankap05@gmail.com>
2020-02-18 22:18:56 +0530

    third commit

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ app.get("/blogs/new", function(req, res){
 	res.render("new");
 })
 
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err)
+			res.redirect("/blogs");
+		else
+			res.render("show", {blog:foundBlog});
+	})
+})
+
 app.post("/blogs", function(req, res){
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err)
