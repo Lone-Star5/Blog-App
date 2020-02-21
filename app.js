f03225662fb192363c6a59f8a3d01dc1bd7bbc96
Lone-Star5 <amankap05@gmail.com>
2020-02-21 10:16:43 +0530

    blog website complete

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 mongoose.set('useNewUrlParser', true);
@@ -58,6 +60,7 @@ Blog.findById(req.params.id, function(err, foundBlog){
 })
 
 app.post("/blogs", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err)
 			res.render("new")
@@ -67,6 +70,7 @@ app.post("/blogs", function(req, res){
 });
 
 app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if(err)
 			res.redirect("/blogs");
